3bb227ab1db94a83947f521bfe73b4b01e528c55
vledoze <v.ledoze@gmail.com>
2017-04-26 18:20:04 +0200

    Evolutions sur la bwd_propagation

--- NeuralNetworkLibFct.py
+++ NeuralNetworkLibFct.py
@@ -20,29 +20,25 @@ class NeuralNetwork:
             vec_sl[0] = nombre d'activation du layer 1
             ...
             vec_sl[__num_L-1] = nombre d'activation du layer __num_L
-        - __mat_pond : matrice des ponderations
-            mat_pond[0] = matrice permettant de passer des entrees au layer 1
+        - __lst_mat_pond : liste des matrices des ponderations
+            lst_mat_pond[0] = matrice permettant de passer des entrees au layer 1
             ...
-            mat_pond[i] = matrice permettant de passer du layer i au layer i+1
+            lst_mat_pond[i] = matrice permettant de passer du layer i au layer i+1
             ...
-            mat_pond[num_L] = matrice permettant de passer du layer num_L aux sorties
+            lst_mat_pond[num_L] = matrice permettant de passer du layer num_L aux sorties
     """
 
-    def __init__(self, num_n=1, num_k=1, vec_nb_a=[1]):
+    def __init__(self, num_n=1, num_k=1, vec_na=[1]):
         """ Constructeur de la classe NeuralNetwork
             entrees : num_n : nombre de classes en entree
                       num_k : nombre de classes en sortie
-                      vec_nb_a : vecteur de repartition des couches d'activation
+                      vec_na : vecteur de repartition des couches d'activation
         """
         self.__num_n = num_n
         self.__num_k = num_k
-        self.__vec_sL = np.concatenate(([num_n], vec_nb_a, [num_k]))
+        self.__vec_sL = np.concatenate(([num_n], vec_na, [num_k]))
         self.__num_L = len(self.__vec_sL) - 1
-        self.__mat_pond = []
-        #TODO ajouter random.gauss sur la matrice theta
-        for i in range(self.__num_L):
-            mat_theta_i = np.ones([self.__vec_sL[i+1], (self.__vec_sL[i]+1)])
-            self.__mat_pond.append(mat_theta_i)
+        self.__lst_mat_pond = [np.matrix(np.ones([self.__vec_sL[i+1], (self.__vec_sL[i]+1)])) for i in range(self.__num_L)]
 
     ### Methodes ------------------------------------------------------------###
     def mtd_fwd_activation(self, vec_a, num_iL):
@@ -51,13 +47,9 @@ class NeuralNetwork:
                      num_iL : numero du layer courant
             sorties: vec_a1 : vecteur des activations suivantes
         """
-        mat_theta = self.__mat_pond[num_iL]
-        num_nb_a = self.__vec_sL[num_iL+1]
-        vec_a1 = []
-        for i in range(num_nb_a):
-            vec_theta = mat_theta[i,:]
-            vec_a1.append(ml.fct_hyp_cls_lin(vec_theta, vec_a))
-        return vec_a1
+        mat_theta = self.__lst_mat_pond[num_iL]
+        num_na = self.__vec_sL[num_iL+1]
+        return [ml.fct_hyp_cls_lin(mat_theta[i,:].A1, vec_a) for i in range(num_na)]
 
     def mtd_bwd_activation(self, vec_a, num_iL):
         """ Fonction de calcul des activations dans le sens backward
@@ -65,13 +57,9 @@ class NeuralNetwork:
                      num_iL : numero du layer courant
             sorties: vec_a1 : vecteur des activations suivantes
         """
-        mat_theta = self.__mat_pond[num_iL-1]
-        num_nb_a = self.__vec_sL[num_iL-1]
-        vec_a0 = []
-        for i in range(1, num_nb_a+1):
-            vec_theta = mat_theta[:,i]
-            vec_a0.append(ml.fct_hyp_cls_lin(vec_theta, vec_a))
-        return vec_a0
+        mat_theta = self.__lst_mat_pond[num_iL-1]
+        num_na = self.__vec_sL[num_iL-1]
+        return [ml.fct_hyp_cls_lin(mat_theta[:,i+1].A1, vec_a) for i in range(num_na)]
 
     def mtd_hyp(self, vec_x):
         """ Fonction hypothese propre au reseau
@@ -104,36 +92,51 @@ class NeuralNetwork:
         num_J1 = -num_J1/num_m
         if num_lambda > 0:
             for l in range(self.__num_L-1):
-                mat_theta = self.__mat_pond[l]
+                mat_theta = self.__lst_mat_pond[l]
                 num_s0 = self.__vec_sL[l]
                 num_s1 = self.__vec_sL[l+1]
                 for i in range(num_s0):
                     for j in range(num_s1):
-                        num_J2 += pow(mat_theta[j][i], 2)
+                        num_J2 += pow(mat_theta[j,i], 2)
             num_J2 = num_J2*num_lambda/(2.0*num_m)
-        return num_J1 + num_J2
+        return num_J1+num_J2
 
-    def mtd_bwd_prpg(self, mat_x, vec_y):
+    def mtd_bwd_prpg(self, mat_x, vec_y, num_lambda=-1):
         """ Algorithme de back-propagation pour calculer la derivee partielle
             du cout du reseau de neurones
             entree : [mat_x, vec_y] : training set
             sortie : num_gradJ      : gradient du cout
         """
         num_m = len(vec_y)
-        mat_grad = []
         mat_out = self.mtd_mat_out(vec_y)
+        mat_x0  = ml.fct_add_ones(mat_x)
+        mat_dlt = np.zeros([self.__num_L-1, num_m, self.__num_n])
         for i in range(num_m):
-            mat_a = [mat_x[i]]
-            for l in range(self.__num_L-1):
-                mat_a.append(self.mtd_fwd_activation(mat_a[l].A1, l))
-            mat_d = [[a-out for a,out in zip(mat_a[self.__num_L-1], mat_out[i])]]
+            #Forward
+            vec_a = mat_x0[i].A1
+            lst_vec_a = [vec_a]
+            for l in range(self.__num_L):
+                vec_a = self.mtd_fwd_activation(vec_a, l)
+                if l < (self.__num_L - 1):
+                    vec_a.insert(0, 1.)
+                lst_vec_a.append(vec_a)
+            #Backward
+            lst_vec_d = [[(a-out) for a,out in zip(lst_vec_a[self.__num_L], mat_out[i])]]
             for l in reversed(range(self.__num_L)):
-                vec_d0 = self.mtd_bwd_activation(mat_d[-1], l+1)
+                vec_dz = self.mtd_bwd_activation(lst_vec_d[0], l+1)
                 pdb.set_trace()
-                vec_d1 = np.dot(vec_d0, mat_a[l])
-                vec_d2 = np.dot(mat_a[l], np.ones(self.__vec_sL[l]))
-                vec_d = np.dot(vec_d1, vec_d2)
-                mat_d.append(vec_d)
+                vec_dg = [a*(1. - a) for a in lst_vec_a[l]]
+                vec_d = [(dz*dg) for dz,dg  in zip(vec_dz, vec_dg)]
+                lst_vec_d.insert(0, vec_d)
+            pdb.set_trace()
+            for l in range(self.__num_L-1):
+                mat_theta = self.__lst_mat_pond[l]
+                for jj in range(self.__vec_sL[l]):
+                    for ii in range(self.__vec_sL[l+1]):
+                        if jj == 0 & num_lambda > 0:
+                            mat_dlt[l, i, j] = mat_dlt[l, i, j] + (lst_vec_a[l][jj]*lst_vec_d[l+1][ii] + num_lambda*mat_theta[ii, jj])/num_m
+                        else:
+                            mat_dlt[l, i, j] = mat_dlt[l, i, j] + (lst_vec_a[l][jj]*lst_vec_d[l+1][ii])/num_m
 
     def mtd_mat_out(self, vec_y):
         """ Transforme un vecteur de sortie en matrice de sortie pour comparer
@@ -146,7 +149,7 @@ class NeuralNetwork:
         for i in range(num_m):
             vec_out = []
             for k in range(self.__num_k):
-                if vec_y[i] == k:
+                if vec_y[i] == k+1:
                     vec_out.append(1)
                 else:
                     vec_out.append(0)
@@ -163,8 +166,8 @@ class NeuralNetwork:
     def set_num_k(self, num_k):
         self.__num_k = num_k
 
-    def set_mat_pond(self, mat_pond):
-        self.__mat_pond = mat_pond
+    def set_lst_mat_pond(self, lst_mat_pond):
+        self.__lst_mat_pond = lst_mat_pond
 
     ### Getter --------------------------------------------------------------###
     def get_num_L(self):
@@ -176,8 +179,8 @@ class NeuralNetwork:
     def get_num_k(self):
         return self.__num_k
 
-    def get_mat_pond(self):
-        return self.__mat_pond
+    def get_lst_mat_pond(self):
+        return self.__lst_mat_pond
 
 if __name__ == "__main__":
     """ Test des fonctions """
@@ -192,7 +195,7 @@ if __name__ == "__main__":
     print "Matrice de ponderation/layer : "
     for i in range(nn_nwk.get_num_L()):
         print "Matrice ", i
-        print nn_nwk.get_mat_pond()[i]
+        print nn_nwk.get_lst_mat_pond()[i]
     mat_x = np.matrix([[1., 2., 3.], [4., 5., 6.], [3., 14., 9.]])
     vec_y = np.array([1., 0., 1.])
     print "Training set"
